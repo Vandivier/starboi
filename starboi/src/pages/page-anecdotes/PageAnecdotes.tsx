0da07c6fc3366b9643272e5458607550eebaa9a3
Vandivier <john@afterecon.com>
2021-08-15 13:40:03 -0400

    feat: handleClickAddAnecdote
    and stub other buttonsection methods

--- starboi/src/pages/page-anecdotes/PageAnecdotes.tsx
+++ starboi/src/pages/page-anecdotes/PageAnecdotes.tsx
@@ -4,15 +4,20 @@ import React from "react"; // TODO: line needed?
 
 import anecdotesToType from "./anecdotes.json";
 
-import type { Anecdote } from "../../customTypes/Anecdote";
+import { Anecdote, EMPTY_ANECDOTE } from "../../customTypes/Anecdote";
 
-const anecdotes: Anecdote[] = anecdotesToType;
+const importedAnecdotes: Anecdote[] = anecdotesToType;
 
 // TODO: anexc
 const generateUniqueKey = (a: Anecdote) => JSON.stringify(a);
 
 type AnecdoteCardProps = {
   anecdote: Anecdote;
+};
+
+type ButtonSectionPropTypes = {
+  anecdotes: Anecdote[];
+  setAnecdotes: React.Dispatch<React.SetStateAction<Anecdote[]>>;
 };
 
 const AnecdoteCard = ({ anecdote }: AnecdoteCardProps) => (
@@ -28,11 +33,44 @@ const AnecdoteCard = ({ anecdote }: AnecdoteCardProps) => (
   </div>
 );
 
-export const PageAnecdotes = () =>
-  Array.isArray(anecdotes) ? (
-    <section className="anecdote-card-section">
-      {anecdotes.map((a) => (
-        <AnecdoteCard anecdote={a} key={generateUniqueKey(a)} />
-      ))}
+const AnecdoteCardSection = ({ anecdotes }: { anecdotes: Anecdote[] }) => (
+  <section className="anecdote-card-section" contentEditable>
+    {anecdotes.map((a) => (
+      <AnecdoteCard anecdote={a} key={generateUniqueKey(a)} />
+    ))}
+  </section>
+);
+
+// TODO: compare to Rockstarboi, who uses Material <Button />
+const ButtonSection = ({ anecdotes, setAnecdotes }: ButtonSectionPropTypes) => {
+  const handleClickAddAnecdote = (
+    ev: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setAnecdotes([{ ...EMPTY_ANECDOTE }].concat(anecdotes));
+  };
+  const handleClickImportAnecdotes = (
+    ev: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {};
+  const handleClickExportAnecdotes = (
+    ev: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {};
+
+  return (
+    <section>
+      <button onClick={handleClickAddAnecdote}>Add Anecdote</button>
+      <button onClick={handleClickImportAnecdotes}>Import Anecdote JSON</button>
+      <button onClick={handleClickExportAnecdotes}>Export Anecdote JSON</button>
     </section>
-  ) : null;
+  );
+};
+
+export const PageAnecdotes = () => {
+  const [anecdotes, setAnecdotes] = React.useState(importedAnecdotes);
+
+  return (
+    <>
+      <ButtonSection anecdotes={anecdotes} setAnecdotes={setAnecdotes} />
+      {anecdotes.length ? <AnecdoteCardSection anecdotes={anecdotes} /> : null}
+    </>
+  );
+};
